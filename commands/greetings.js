963ed15fc889aa3d05133d43e9fa1bd118097e81
Ertan Muslic <ertanmuslic@gmail.com>
2022-04-26 18:02:51 +0200

    Bot

--- commands/greetings.js
+++ commands/greetings.js
@@ -7,5 +7,22 @@ module.exports = {
 	args:false,
 	usage:false,
 	execute(msg, args) {
+        var randomnumber = Math.floor(Math.random() * 5);
+
+        switch(randomnumber){
+            case 1:
+                return msg.channel.send(`Greetings fellow human called ${msg.author}`);
+            case 2:
+                return msg.channel.send(`Hello old friend,i hope youre still called ${msg.author.username}`);
+            case 3:
+                return msg.channel.send(`You again ${msg.author},get lost`);
+            case 4:
+                return msg.channel.send(`Greetings fellow human called ${msg.author}`);
+            case 5:
+                return msg.channel.send(`Greetings fellow human called ${msg.author}`);
+            
+        }
+
+        msg.channel.send(`Greetings ${msg.author.name}. Nice to meet you`);
     },
 };
