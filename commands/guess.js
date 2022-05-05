c5c9bdc4b4f64820914c3a5875629fc67d84d2ef
Ertan Muslic <ertanmuslic@gmail.com>
2022-05-05 18:59:07 +0200

    .

--- commands/guess.js
+++ commands/guess.js
@@ -10,18 +10,28 @@ module.exports = {
 	// eslint-disable-next-line no-unused-vars
 	execute(msg, args) {
         const argument = args[0];
-            if(randomNumer === undefined){
-                randomNumer = Math.floor(Math.random() * 10);
-                return msg.reply('The game has started. Please guess the number');
-            }
-            else{
-                if(randomNumer === parseInt(argument)){
-                    randomNumer = undefined;
-                    return msg.reply("Congrats, you won.");
-                }
-                else{
-                    return msg.reply("You stupid");
-                }
-            }
+        if(argument == 'start'){
+            randomNumer = Math.floor(Math.random() * 10);
+            return msg.reply('The new game has started. Please guess the number');
+        }
+        if(randomNumer == undefined){
+            return msg.reply("Please start the game by typing start");
+        }
+
+        if(randomNumer === parseInt(argument)){
+            randomNumer = undefined;
+            return msg.reply("Congrats, you won.");
+        }
+
+        else{
+            return msg.reply("You stupid");
+        }
+
+        // if(randomNumer === undefined){
+        //         randomNumer = Math.floor(Math.random() * 10);
+        //         return msg.reply('The game has started. Please guess the number');
+        //     }
+        // else{
+        //     }
 	},
 };
